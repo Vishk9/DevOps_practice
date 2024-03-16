5eb086b2723caa58a12f53646bb37a39083a8ab0
Vishk <Vishkumavat5@gmail.com>
2024-03-16 15:09:18 +0530

    stash done

--- demo.sh
+++ demo.sh
@@ -1,7 +1,14 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 
+=======
+=======
+>>>>>>> Stashed changes
 thoda kaam krlo
 
 i am select * from t_Sbi
 >>>>>>> Stashed changes
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
